510d54bd1f0331ac2972f7e49e94caeb19d033e6
ahmedbhy1 <ahmedbhy1@gmail.com>
2025-11-12 17:56:23 +0100

    add  operation for adding init.sql

--- docker/dags/imdb_reddit_pipeline.py
+++ docker/dags/imdb_reddit_pipeline.py
@@ -1,14 +1,28 @@
 from airflow import DAG
+from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.operators.python import PythonOperator
 from datetime import datetime
-from main import run_pipeline  
+from main import run_pipeline
 
-with DAG("imdb_reddit_pipeline",
-         start_date=datetime(2025,1,1),
-         schedule_interval="@daily",
-         catchup=False) as dag:
+with DAG(
+    "imdb_reddit_pipeline",
+    start_date=datetime(2025, 1, 1),
+    schedule_interval="@daily",
+    catchup=False
+) as dag:
 
+    # Task 0: Initialize database
+    init_db = PostgresOperator(
+        task_id="init_db",
+        postgres_conn_id="postgres_default",  # make sure this connection exists in Airflow
+        sql="/opt/airflow/dags/init.sql"
+    )
+
+    # Task 1: Run ETL
     t1 = PythonOperator(
-        task_id="run_imdb_reddit_etl",
+        task_id="run_imdb_etl",
         python_callable=run_pipeline
     )
+
+    # Set dependency: init_db must run before t1
+    init_db >> t1
